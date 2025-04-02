85fde78302c955a054f3bf65ebe4a88c096a3bb5
Thiago Rangel <108483500+ThiagoRangel45@users.noreply.github.com>
2025-04-02 12:27:08 -0300

    Delete build directory

--- build/CMakeFiles/RPC_Simulation.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/RPC_Simulation"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/RPC_Simulation.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/CMakeFiles/RPC_Simulation.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty custom commands generated dependencies file for RPC_Simulation.
-# This may be replaced when dependencies are built.
